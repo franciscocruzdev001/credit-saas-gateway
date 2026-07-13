bd9fbf3d2407a9be2dbeb30a10e74321042030e9
DESKTOP-8125QUT\francisco <francisco.cruz.dev@gmail.com>
2026-07-13 12:23:22 -0600

    [CSG-01] - Add Cedit MongoModel and inject depencies

---
+++ services/credits/src/gateway/CreditMongoModel.ts
@@ -0,0 +1,15 @@
+import { injectable } from "inversify";
+import { CreditsModel, ICredits } from "../schema/mongodb/CreditsModel";
+import { BaseMongoModel } from "./BaseMongoModel";
+
+@injectable()
+export class CreditMongoModel extends BaseMongoModel<ICredits> {
+  constructor() {
+    // Inyectamos explícitamente el modelo Mongoose de manera estática a la clase superior
+    super(CreditsModel);
+  }
+  // Aquí puedes añadir métodos específicos que solo pertenezcan a User
+  /*public async findByEmail(email: string): Promise<IUserDocument | null> {
+    return this.model.findOne({ email }).exec();
+  }*/
+}
